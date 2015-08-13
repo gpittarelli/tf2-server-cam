1d6135d3bd139ff2988e3757d6a97205cc338bf9
George Pittarelli <gjp@cs.umd.edu>
2015-08-13 03:26:55 -0400

    servers load better if we skip :other, and also ssq query every server

--- src/tf2_server_cam/core.clj
+++ src/tf2_server_cam/core.clj
@@ -1,20 +1,39 @@
 (ns tf2-server-cam.core
   (:require [clj-ssq.core :as ssq]
-            [clj-ssq.codecs :refer [msq-regions]])
+            [clj-ssq.codecs :refer [msq-regions]]
+            [clojure.string :as str])
+  (:import [java.net InetAddress])
   (:gen-class))
 
-(def regions (vals msq-regions))
+(def regions (disj (set (vals msq-regions)) :other))
+
+(def master-ips
+  (->> "hl2master.steampowered.com"
+       (InetAddress/getAllByName)
+       vec
+       (map #(.getHostAddress %))))
 
 (defn- list-tf2-servers [region]
-  (loop []
-    (let [servers
-          @(ssq/master "hl2master.steampowered.com" 27011
-                       region "\\appid\\440"
-                       :timeout 20000 :socket-timeout 20000)]
-      (if (:err servers)
-        (do (Thread/sleep 30000)
-            (recur))
-        servers))))
+  (let [servers
+        (loop [ips (cycle master-ips)]
+          (let [[cur-ip & rest-ips] ips
+                t (println "try" region cur-ip)
+                servers
+                @(ssq/master cur-ip 27011
+                             region "\\appid\\440"
+                             :timeout 20000 :socket-timeout 20000)]
+            (if (:err servers)
+              (do (Thread/sleep 30000)
+                  (recur rest-ips))
+              servers)))]
+    (->> servers
+         (map #(str/split % ":"))
+         (map (fn [[ip port]]
+                {:ip ip
+                 :port port
+                 :info (ssq/info ip port)
+                 :rules (ssq/rules ip port)
+                 :players (ssq/players ip port)})))))
 
 (defn -main [& args]
   (let [region->servers
