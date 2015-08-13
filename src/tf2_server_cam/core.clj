0a6e635b2b995532261aaa39f08aa0b70e20dbb8
George Pittarelli <gjp@cs.umd.edu>
2015-08-13 02:19:08 -0400

    Basic master server querying for all regions

--- src/tf2_server_cam/core.clj
+++ src/tf2_server_cam/core.clj
@@ -1,7 +1,20 @@
 (ns tf2-server-cam.core
+  (:require [clj-ssq.core :as ssq]
+            [clj-ssq.codecs :refer [msq-regions]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def regions (vals msq-regions))
+
+(defn- list-tf2-servers [region]
+  (ssq/master "hl2master.steampowered.com" 27011 region "\\appid\\440" :timeout 10000 :socket-timeout 10000))
+
+(defn -main [& args]
+  (let [region->servers
+        (zipmap regions
+                (->> regions
+                     (map list-tf2-servers)
+                     doall
+                     (map deref)
+                     doall))]
+    (println region->servers)
+    (shutdown-agents)))
