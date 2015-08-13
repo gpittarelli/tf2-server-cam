edcc23c7b9cdaaa31d90167b0104925075ea1916
George Pittarelli <gjp@cs.umd.edu>
2015-08-13 02:40:22 -0400

    increase timeout even further

--- src/tf2_server_cam/core.clj
+++ src/tf2_server_cam/core.clj
@@ -6,16 +6,15 @@
 (def regions (vals msq-regions))
 
 (defn- list-tf2-servers [region]
-  @(future
-     (loop []
-       (let [servers
-             @(ssq/master "hl2master.steampowered.com" 27011
-                          region "\\appid\\440"
-                          :timeout 10000 :socket-timeout 10000)]
-         (if (:err servers)
-           (do (Thread/sleep 10000)
-               (recur))
-           servers)))))
+  (loop []
+    (let [servers
+          @(ssq/master "hl2master.steampowered.com" 27011
+                       region "\\appid\\440"
+                       :timeout 20000 :socket-timeout 20000)]
+      (if (:err servers)
+        (do (Thread/sleep 30000)
+            (recur))
+        servers))))
 
 (defn -main [& args]
   (let [region->servers
