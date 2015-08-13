2adb392384a20b290bf34a50f3a21d28003d5bf4
George Pittarelli <gjp@cs.umd.edu>
2015-08-13 13:42:01 -0400

    move to clj-ssq v0.4.1 to have server keywords as strings

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-ssq "0.4.0"]]
+                 [clj-ssq "0.4.1"]]
   :main ^:skip-aot tf2-server-cam.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
